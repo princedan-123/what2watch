65fe5d5c06108ef9a410b0c8da692f8c4db0973a
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-10-11 18:15:16 +0100

    initialized tv show search, added pagination and sorting

--- routers/movie.py
+++ routers/movie.py
@@ -5,10 +5,13 @@ import httpx
 
 movie_router = APIRouter(prefix='/movie', tags=['movie'])
 @movie_router.get('/search')
-async def search_movie(query: str):
+async def search_movie(query: str, page:int = 1):
     tmdb_key = load_env.TMDB_API_KEY
     print(tmdb_key)
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"https://api.themoviedb.org/3/search/movie?api_key={tmdb_key}&query={query}"
+        response = await client.get(f"https://api.themoviedb.org/3/search/movie?api_key={tmdb_key}&query={query}&page={page}"
         )
-        return response.json()
+        payload = response.json()
+        #  sort search items based on popularity
+        payload['results'].sort(key=lambda search_item:search_item['popularity'], reverse=True)
+        return payload
--- routers/tv_shows.py
+++ routers/tv_shows.py
@@ -1,4 +1,17 @@
 """Router for tv shows."""
 from fastapi import APIRouter
+from core.settings import load_env
+import httpx
 
 tv_show_router = APIRouter(prefix='/tvshow', tags=['tvshows'])
+@tv_show_router.get('/search')
+async def search_tvshow(query: str, page:int = 1):
+    tmdb_key = load_env.TMDB_API_KEY
+    print(tmdb_key)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"https://api.themoviedb.org/3/search/tv?api_key={tmdb_key}&query={query}&page={page}"
+        )
+        payload = response.json()
+        #  sort search items based on popularity
+        payload['results'].sort(key=lambda search_item:search_item['popularity'], reverse=True)
+        return payload
