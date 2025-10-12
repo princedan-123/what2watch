3967ebaa091cd44aacfd0be9bd02d4fc46d362de
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-10-12 19:31:52 +0100

    implement get tvsow

--- routers/tv_shows.py
+++ routers/tv_shows.py
@@ -3,10 +3,10 @@ from fastapi import APIRouter, HTTPException
 from core.settings import load_env
 import httpx
 
+tmdb_key = load_env.TMDB_API_KEY
 tv_show_router = APIRouter(prefix='/tvshow', tags=['tvshows'])
 @tv_show_router.get('/search', status_code=200)
 async def search_tvshow(query: str, page:int = 1):
-    tmdb_key = load_env.TMDB_API_KEY
     async with httpx.AsyncClient() as client:
         base_url = 'https://api.themoviedb.org/3/search/tv'
         query_parameter = f'?api_key={tmdb_key}&query={query}&page={page}'
@@ -17,3 +17,15 @@ async def search_tvshow(query: str, page:int = 1):
         #  sort search items based on popularity
         payload['results'].sort(key=lambda search_item:search_item['popularity'], reverse=True)
         return payload
+
+@tv_show_router.get('/get/{tv_id}', status_code=200)
+async def get_tvshow(tv_id:int):
+    """An endpoint that fetches a specific tvshow."""
+    async with httpx.AsyncClient() as client:
+        base_url = 'https://api.themoviedb.org/3/tv/'
+        response = await client.get(f'{base_url}{tv_id}?api_key={tmdb_key}')
+        if not response.json():
+            raise HTTPException(
+                status_code=500, detail='Oops an error occured!!'
+                )
+        return response.json()
