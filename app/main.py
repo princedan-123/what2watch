57ce60a176174767b616daef5d439189c182b648
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-12-30 16:42:27 +0100

    implement COR

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@
 from fastapi import FastAPI
 from routers.movie import movie_router
 from routers.tv_shows import tv_show_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
 app.include_router(movie_router)
 app.include_router(tv_show_router)
