1007b2a38f5d0b9f12d7e705ccc642c3c7c50865
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2026-01-25 17:05:24 +0100

    updated CORS restriction

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://what2watchcom.vercel.app",
+        "https://what2watchcom.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
