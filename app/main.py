41bbf271dcd04e2bbd534509fba9af5d0a897e3a
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2026-01-29 15:36:27 +0100

    updated COR

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ from routers.tv_shows import tv_show_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",              # local React / Vite / Next dev
+    "http://127.0.0.1:3000",
+    "https://what2watch-sandy.vercel.app", # your Vercel frontend
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://what2watch-sandy.vercel.app/",
-        "http://localhost:5173/",
-        "*"
-    ],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True
